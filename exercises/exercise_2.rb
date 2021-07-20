8fe66ec49f0b2c052daedacbbb3fcbb875b53b0d
Mingfeng Li <lmf950122@gmail.com>
2021-07-20 04:54:00 +0000

    Find the store based on id and update one store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# default id 
+# binding.pry
+@store1 = Store.find(id = 1)
+@store2 = Store.find(id = 2)
+
+@store1.update(name: "Victoria")
+
+p @store1
